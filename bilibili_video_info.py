6442fc03bcb33c0b673a54bed7e91e6ce36341dc
AwAqwpAwA <103885743+AwAqwpAwA@users.noreply.github.com>
2023-03-02 03:13:35 +0800

    Add files via upload

---
+++ bilibili_video_info.py
@@ -0,0 +1,39 @@
+#感谢 https://github.com/SocialSisterYi/bilibili-API-collect 提供API文档!
+#by cueavyqwp(cy)/AwAqwpAwA
+#https://space.bilibili.com/500328587
+#https://github.com/AwAqwpAwA
+#哔哩哔哩干杯!
+import requests , json , time
+
+def T(Time):return time.strftime("%Y/%m/%d %H:%M:%S",time.localtime(Time))#秒级时间戳转字符串
+
+while 1:
+    Get="?"
+    av_or_bv=input("请输入AV号或BV号>>>")
+    if len(av_or_bv) < 3 : continue
+    if av_or_bv[:2] in ["BV","bv","Bv","bV"]:Get+=f"bvid=BV{av_or_bv[2:]}"
+    elif av_or_bv[:2] in ["AV","av","Av","aV"]:Get+=f"aid={av_or_bv[2:]}"
+    else:Get+=f"aid={av_or_bv}"
+    response=requests.get(f"https://api.bilibili.com/x/web-interface/view{Get}")
+    List=json.loads(response.content)
+    if List["code"] != 0:
+        print(List["code"],{-400:"请求错误",-403:"权限不足",-404:"无视频",62002:"稿件不可见",62004:"稿件审核中"}[List["code"]])
+        continue
+    d=List["data"]
+    print(f"""
+{'==='*30}
+标题:[{d['title']}]
+aid: {d['aid']}
+bvid: {d['bvid']}
+类型: {['自制','搬运'][d['copyright']-1]}
+稿件发布时间: {T(d['pubdate'])}
+用户投稿时间: {T(d['ctime'])}
+封面: {d['pic']}
+简介: 
+{'---'*30}
+{d['desc']}
+{'---'*30}
+作者: {d['owner']['name']}
+作者UID: {d['owner']['mid']}
+作者头像: {d['owner']['face']}
+{'==='*30}""")
